0e2796d28042260f9536f902872b105de2679b24
Akomi <87880680+akomi-dev@users.noreply.github.com>
2023-06-19 13:32:25 -0400

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,58 @@ export default function App() {
   return (
     <div className="App">
       <div className="Container" >
-        <header className='Head'>
+
+        <header>
           <img 
               className='mainImg' 
               alt='profile img' 
               src={img}
+              height={200}
           />
           <h1 className="Name">Akomi</h1>
+          <p className='description'>
+            eros donec ac odio tempor orci dapibus ultrices in iaculis 
+            nunc sed augue lacus viverra vitae congue eu consequat ac
+          </p>
         </header>
+
+        <div className='links'>
+
+          <a href="https://github.com/akomi-dev">
+            <div className="GitHub">
+              GitHub
+            </div>
+          </a>
+
+          <a href='https://www.linkedin.com/in/akomidev/'>
+            <div className="LinkedIn">
+              LinkedIn
+            </div>
+          </a>
+
+          <a href='https://discordapp.com/users/465709255974977546'>
+            <div className="Discord">
+              Discord
+            </div>
+          </a>
+
+          <a href='https://www.instagram.com/akomi.dev/'>
+            <div className="Instagram">
+              Instagram
+            </div>
+          </a>
+
+          <a href='https://twitter.com/akomi_dev'>
+            <div className="Twitter">
+              Twitter
+            </div>
+          </a>
+        </div>
+
+        <footer>
+          <div className='foot'>Footer</div>
+        </footer>
+
       </div>
     </div>
   );
